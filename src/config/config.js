35d9bbe247dedbe4a7bfbeafc87646febcd33159
yanxue <395535065@qq.com>
2019-06-18 18:10:31 +0800

    订正地址

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ export default {
     proxy: [
         {
             host: 'https://192.168.31.66:9090',
-            projectName: '/customer-made-web'
+            projectName: '/web'
         }
     ]
 }
